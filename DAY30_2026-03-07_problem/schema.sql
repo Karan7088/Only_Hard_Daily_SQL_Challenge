c0fc39edb2366569e9dff19e6b345c9d70caf21b
Karan7088 <161558653+Karan7088@users.noreply.github.com>
2026-03-07 17:09:19 +0530

    Add schema for seats and passengers tables

--- DAY30_2026-03-07_problem/schema.sql
+++ DAY30_2026-03-07_problem/schema.sql
@@ -1,1 +1,15 @@
- 
+CREATE TABLE seats (
+seat_id INT,
+seat_class VARCHAR(20),
+seat_row INT
+);
+
+
+
+CREATE TABLE passengers (
+passenger_id INT,
+ticket_class VARCHAR(20),
+loyalty_status VARCHAR(20),
+checkin_time TIMESTAMP
+);
+
