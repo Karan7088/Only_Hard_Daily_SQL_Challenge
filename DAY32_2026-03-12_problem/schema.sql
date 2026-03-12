53340e4da7e27eaabebd95f140fe46fc8dd15693
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-03-12 14:44:09 +0530

    Update schema.sql

--- DAY32_2026-03-12_problem/schema.sql
+++ DAY32_2026-03-12_problem/schema.sql
@@ -1,1 +1,13 @@
- 
+CREATE TABLE listings (
+    listing_id INT PRIMARY KEY,
+    city VARCHAR(50),
+    base_price INT
+);
+
+CREATE TABLE bookings (
+    booking_id INT PRIMARY KEY,
+    listing_id INT,
+    booking_date DATE
+);
+
+
