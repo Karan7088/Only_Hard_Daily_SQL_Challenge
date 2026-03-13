286b2c1b37e4b0c66e47a99ff9a93832f02c116e
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-03-13 15:08:38 +0530

    Add SQL query to find conflicting robot tasks

--- DAY33_2026-03-13_problem/solution.sql
+++ DAY33_2026-03-13_problem/solution.sql
@@ -1,1 +1,14 @@
- 
+ SELECT 
+    a.robot_id,
+    a.task_id AS task1,
+    b.task_id AS task2,
+    b.start_time AS conflict_start,
+    a.end_time AS conflict_end
+FROM tasks a
+INNER JOIN tasks b
+    ON a.robot_id = b.robot_id
+   AND a.task_id < b.task_id
+   AND a.end_time > b.start_time
+ORDER BY 
+    a.robot_id,
+    a.task_id;
