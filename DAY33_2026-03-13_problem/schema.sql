b15ad4a7688c9d280ec951ddb18c969d7d543bd0
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-03-13 15:02:26 +0530

    Update schema.sql

--- DAY33_2026-03-13_problem/schema.sql
+++ DAY33_2026-03-13_problem/schema.sql
@@ -1,1 +1,11 @@
- 
+CREATE TABLE robots (
+robot_id INT,
+robot_name VARCHAR(50)
+);
+CREATE TABLE tasks (
+task_id INT,
+robot_id INT,
+start_time DATETIME,
+end_time DATETIME,
+zone VARCHAR(20)
+);
