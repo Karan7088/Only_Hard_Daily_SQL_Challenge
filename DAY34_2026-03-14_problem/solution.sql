0bc0104561c4d999ec8206c19db700d0bf4ef7c7
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-03-14 15:43:33 +0530

    Implement SQL query for required gates calculation
    Added a SQL query to calculate the required number of gates based on flight events.

--- DAY34_2026-03-14_problem/solution.sql
+++ DAY34_2026-03-14_problem/solution.sql
@@ -1,1 +1,47 @@
- 
+ WITH events AS (
+    SELECT flight_id, arrival_time AS event_time
+    FROM flights
+    
+    UNION
+    
+    SELECT flight_id, departure_time AS event_time
+    FROM flights
+),
+
+event_flags AS (
+    SELECT 
+        flight_id,
+        event_time,
+        CASE 
+            WHEN ROW_NUMBER() OVER (PARTITION BY flight_id ORDER BY event_time) = 1 
+            THEN 1 
+            ELSE -1 
+        END AS change_flag
+    FROM events
+),
+
+adjusted_events AS (
+    SELECT 
+        event_time,
+        CASE 
+            WHEN TIMESTAMPDIFF(SECOND, event_time, LAG(event_time) OVER ()) = 0
+                 AND change_flag + LAG(change_flag) OVER () = 0
+            THEN 0
+            ELSE change_flag
+        END AS change_flag
+    FROM event_flags
+),
+
+running_gates AS (
+    SELECT 
+        event_time,
+        SUM(change_flag) OVER (
+            ORDER BY event_time
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) AS active_gates
+    FROM adjusted_events
+)
+
+SELECT 
+    MAX(active_gates) AS required_gates
+FROM running_gates;
