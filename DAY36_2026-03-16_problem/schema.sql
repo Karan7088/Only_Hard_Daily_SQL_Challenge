8580e81f4bb49ccb776d6ba76f9d02d263d85c49
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-03-16 13:23:07 +0530

    Update schema.sql

--- DAY36_2026-03-16_problem/schema.sql
+++ DAY36_2026-03-16_problem/schema.sql
@@ -1,1 +1,15 @@
- 
+  CREATE TABLE watch_sessions (
+    session_id INT PRIMARY KEY,
+    user_id INT,
+    video_id INT,
+    watch_time INT,
+    video_length INT,
+    watch_date DATE
+);
+
+CREATE TABLE video_engagement (
+    video_id INT PRIMARY KEY,
+    likes INT,
+    comments INT,
+    shares INT
+);
