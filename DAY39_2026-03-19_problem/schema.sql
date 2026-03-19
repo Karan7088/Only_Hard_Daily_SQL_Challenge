7e96b75c3c31c4f80119d3d418f589db1403d617
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-03-19 16:40:37 +0530

    Update schema.sql

--- DAY39_2026-03-19_problem/schema.sql
+++ DAY39_2026-03-19_problem/schema.sql
@@ -1,1 +1,7 @@
- 
+ CREATE TABLE orders_raw (
+    order_id INT,
+    customer_id INT,
+    product_ids VARCHAR(255),     -- comma separated
+    quantities VARCHAR(255),      -- comma separated
+    prices VARCHAR(255)           -- comma separated
+);
