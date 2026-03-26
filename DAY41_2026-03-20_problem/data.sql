1199bf4cc1dcc971291ea4815497ac8743ef912d
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-03-26 13:05:22 +0530

    Update data.sql

--- DAY41_2026-03-20_problem/data.sql
+++ DAY41_2026-03-20_problem/data.sql
@@ -1,1 +1,31 @@
- 
+ INSERT INTO sales VALUES
+-- Electronics
+(1, 'Electronics', 500, '2024-01-01'),
+(1, 'Electronics', 300, '2024-01-05'),
+
+(2, 'Electronics', 800, '2024-01-02'),
+
+(3, 'Electronics', 400, '2024-01-03'),
+(3, 'Electronics', 400, '2024-01-06'),
+
+(4, 'Electronics', 900, '2024-01-04'),
+
+-- Clothing
+(5, 'Clothing', 200, '2024-01-01'),
+(5, 'Clothing', 200, '2024-01-02'),
+
+(6, 'Clothing', 700, '2024-01-03'),
+
+(7, 'Clothing', 700, '2024-01-04'),
+
+(8, 'Clothing', 100, '2024-01-05'),
+
+-- Grocery (low revenue category - should get filtered out)
+(9, 'Grocery', 100, '2024-01-01'),
+(10, 'Grocery', 150, '2024-01-02'),
+
+-- Furniture
+(11, 'Furniture', 1000, '2024-01-01'),
+(12, 'Furniture', 800, '2024-01-02'),
+(13, 'Furniture', 800, '2024-01-03'),
+(14, 'Furniture', 600, '2024-01-04'); 
