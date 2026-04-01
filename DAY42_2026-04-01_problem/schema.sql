1175e55e3410945c17832ebdda4165718b68990e
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-04-01 14:48:48 +0530

    Update schema.sql

--- DAY42_2026-04-01_problem/schema.sql
+++ DAY42_2026-04-01_problem/schema.sql
@@ -7,4 +7,11 @@ CREATE TABLE Products (
     product_id VARCHAR(10),
     launch_date DATE
 );
- 
+ CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    product_id VARCHAR(10),
+    order_date DATE
+);
+
+
