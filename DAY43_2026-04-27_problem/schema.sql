d4d43a79fbfe6a89d57200f57046d600e6b5ef9e
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-04-27 11:32:43 +0530

    Update schema.sql

--- DAY43_2026-04-27_problem/schema.sql
+++ DAY43_2026-04-27_problem/schema.sql
@@ -1,1 +1,17 @@
- 
+ DROP TABLE IF EXISTS product_price_history;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE product_price_history (
+    product_id INT,
+    price DECIMAL(10,2),
+    start_time DATETIME,
+    end_time DATETIME
+);
+
+CREATE TABLE orders (
+    order_id INT,
+    product_id INT,
+    order_time DATETIME,
+    quantity INT,
+    price_charged DECIMAL(10,2)
+);
