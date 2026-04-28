f54fba634b93dd019a3ecf3acafa31373f7ad351
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-04-28 13:46:33 +0530

    Update schema.sql

--- DAY44_2026-04-28_problem/schema.sql
+++ DAY44_2026-04-28_problem/schema.sql
@@ -1,1 +1,20 @@
- 
+DROP TABLE IF EXISTS refunds;
+DROP TABLE IF EXISTS orders;
+-- 🔻 DROP TABLES
+-- 💣 INSERT DATA (Brutal Test Cases)
+-- 🏗️ CREATE TABLES
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    order_date DATE,
+    order_amount DECIMAL(10,2)
+);
+
+CREATE TABLE refunds (
+    refund_id INT PRIMARY KEY,
+    order_id INT,
+    refund_date DATE,
+    refund_amount DECIMAL(10,2)
+); 
