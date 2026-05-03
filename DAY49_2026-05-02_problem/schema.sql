dd94778db47896697ef4b8c29e9a0f62a84f188c
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-03 13:56:34 +0530

    Update schema.sql

--- DAY49_2026-05-02_problem/schema.sql
+++ DAY49_2026-05-02_problem/schema.sql
@@ -1,1 +1,10 @@
- 
+ DROP TABLE IF EXISTS flights;
+
+CREATE TABLE flights (
+    flight_id INT,
+    source VARCHAR(10),
+    destination VARCHAR(10),
+    departure_time DATETIME,
+    arrival_time DATETIME,
+    price INT
+);
