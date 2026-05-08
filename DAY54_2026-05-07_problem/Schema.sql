af2c6d3efd0afa1a8a64dfe06c75d4c94910e04e
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-08 09:14:06 +0530

    Update Schema.sql

--- DAY54_2026-05-07_problem/Schema.sql
+++ DAY54_2026-05-07_problem/Schema.sql
@@ -1,1 +1,10 @@
-
+create table delivery_orders(
+order_id int,
+driver_id int,
+customer_id int,
+restaurant_id int,
+order_time datetime,
+delivery_time datetime,
+delivery_status varchar(20),
+order_amount decimal(10,2)
+);
