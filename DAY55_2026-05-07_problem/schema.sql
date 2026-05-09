215bf64b464e43a4de315ec0b49ba2d66248bfb7
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-09 11:22:31 +0530

    Update schema.sql

--- DAY55_2026-05-07_problem/schema.sql
+++ DAY55_2026-05-07_problem/schema.sql
@@ -1,1 +1,9 @@
- 
+DROP TABLE IF EXISTS delivery_events;
+
+CREATE TABLE delivery_events (
+    order_id        INT,
+    event_type      VARCHAR(50),
+    event_time      DATETIME,
+    rider_id        INT,
+    city            VARCHAR(50)
+);
