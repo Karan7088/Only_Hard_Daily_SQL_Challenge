2098a4f4c11874d9385e31f59e09e6ba9aa482fe
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-12 11:02:25 +0530

    Update schema.sql

--- DAY58_2026-05-11_problem/schema.sql
+++ DAY58_2026-05-11_problem/schema.sql
@@ -1,1 +1,9 @@
- 
+DROP TABLE IF EXISTS inventory_snapshots;
+
+CREATE TABLE inventory_snapshots (
+    snapshot_id INT,
+    product_id INT,
+    snapshot_time DATETIME,
+    stock_quantity INT
+);
+
