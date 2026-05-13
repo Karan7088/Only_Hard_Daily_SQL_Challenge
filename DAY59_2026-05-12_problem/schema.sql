7dbd0b13b92c499143a1ab42fe410e18340fa247
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-13 14:04:52 +0530

    Update schema.sql

--- DAY59_2026-05-12_problem/schema.sql
+++ DAY59_2026-05-12_problem/schema.sql
@@ -1,1 +1,10 @@
- 
+ DROP TABLE IF EXISTS elevator_logs;
+
+CREATE TABLE elevator_logs (
+    log_id INT,
+    elevator_id VARCHAR(10),
+    log_time DATETIME,
+    floor_no INT,
+    passenger_count INT
+);
+
