ccfdeab021b09e2d211f84c39976f0de1af70d4b
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-14 11:57:33 +0530

    Update schema.sql

--- DAY60_2026-05-13_problem/schema.sql
+++ DAY60_2026-05-13_problem/schema.sql
@@ -1,1 +1,12 @@
- 
+ DROP TABLE IF EXISTS account_events;
+
+CREATE TABLE account_events (
+    event_id INT PRIMARY KEY,
+    account_id INT,
+    event_time DATETIME,
+    event_type VARCHAR(30),
+    amount DECIMAL(10,2),
+    reference_id VARCHAR(50),
+    status VARCHAR(20)
+);
+
