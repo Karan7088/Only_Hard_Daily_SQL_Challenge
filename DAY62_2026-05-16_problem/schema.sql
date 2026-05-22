a632f23755540a160e53a4938a6b612a674a5ca7
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-22 11:38:27 +0530

    Update schema.sql

--- DAY62_2026-05-16_problem/schema.sql
+++ DAY62_2026-05-16_problem/schema.sql
@@ -1,1 +1,17 @@
- 
+ DROP TABLE IF EXISTS ride_requests;
+DROP TABLE IF EXISTS driver_events;
+
+CREATE TABLE ride_requests (
+    request_id INT,
+    city VARCHAR(50),
+    request_time DATETIME
+);
+
+CREATE TABLE driver_events (
+    event_id INT,
+    driver_id INT,
+    city VARCHAR(50),
+    event_time DATETIME,
+    status VARCHAR(20) -- ONLINE / OFFLINE
+);
+
