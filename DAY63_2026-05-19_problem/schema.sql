53f7205a7c255676c28c303ef393ff761323e90e
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-23 14:12:08 +0530

    Update schema.sql

--- DAY63_2026-05-19_problem/schema.sql
+++ DAY63_2026-05-19_problem/schema.sql
@@ -1,1 +1,12 @@
- 
+DROP TABLE IF EXISTS inventory_audits;
+
+CREATE TABLE inventory_audits (
+    audit_id INT,
+    warehouse_id VARCHAR(10),
+    product_id VARCHAR(10),
+    audit_time DATETIME,
+    system_stock INT,
+    physical_stock INT,
+    adjustment_done VARCHAR(5)
+);
+
