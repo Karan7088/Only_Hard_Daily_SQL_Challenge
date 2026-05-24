afd85aee6bf4a29d17d4cbff9964fd7902f2c078
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-24 10:43:54 +0530

    Create payment_attempts table in data.sql

--- DAY64_2026-05-20_problem/data.sql
+++ DAY64_2026-05-20_problem/data.sql
@@ -1,1 +1,12 @@
- 
+  DROP TABLE IF EXISTS payment_attempts;
+
+CREATE TABLE payment_attempts (
+    txn_id INT,
+    user_id INT,
+    merchant_id VARCHAR(10),
+    amount DECIMAL(10,2),
+    txn_time DATETIME,
+    status VARCHAR(20),
+    failure_reason VARCHAR(50)
+);
+
