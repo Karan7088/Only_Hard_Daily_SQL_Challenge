1a10a4b830639e2505f8db9778251b891f0c745a
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-25 10:37:44 +0530

    Update schema.sql

--- DAY65_2026-05-21_problem/schema.sql
+++ DAY65_2026-05-21_problem/schema.sql
@@ -1,1 +1,19 @@
- 
+  DROP TABLE IF EXISTS subscription_usage;
+DROP TABLE IF EXISTS subscriptions;
+
+CREATE TABLE subscriptions (
+    subscription_id INT,
+    customer_id INT,
+    plan_name VARCHAR(20),
+    start_date DATE,
+    renewal_date DATE,
+    status VARCHAR(20)
+);
+
+CREATE TABLE subscription_usage (
+    usage_id INT,
+    customer_id INT,
+    usage_date DATE,
+    minutes_used INT
+);
+
