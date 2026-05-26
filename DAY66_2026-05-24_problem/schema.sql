dc388c8429e5c2d9ead4f7c8a3e63a5bbd5c500b
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-26 10:25:13 +0530

    Update schema.sql

--- DAY66_2026-05-24_problem/schema.sql
+++ DAY66_2026-05-24_problem/schema.sql
@@ -1,1 +1,10 @@
- 
+ DROP TABLE IF EXISTS employee_meetings;
+
+CREATE TABLE employee_meetings (
+    meeting_id INT,
+    employee_id INT,
+    meeting_title VARCHAR(100),
+    start_time DATETIME,
+    end_time DATETIME
+);
+
