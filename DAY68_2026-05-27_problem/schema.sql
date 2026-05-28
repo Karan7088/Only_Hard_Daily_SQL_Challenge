8e447628aceed5ad800b14c885468174fe3ca9ab
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-28 13:04:54 +0530

    Update schema.sql

--- DAY68_2026-05-27_problem/schema.sql
+++ DAY68_2026-05-27_problem/schema.sql
@@ -1,1 +1,10 @@
- 
+DROP TABLE IF EXISTS tournament_matches;
+CREATE TABLE tournament_matches (
+    match_id INT,
+    match_date DATE,
+    team_a VARCHAR(20),
+    team_b VARCHAR(20),
+    team_a_score INT,
+    team_b_score INT
+);
+
