a4719b91b092fe7cac627722e5718b7486fc7fdb
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-05-31 13:06:36 +0530

    Update schema.sql

--- DAY70_2026-05-29_problem/schema.sql
+++ DAY70_2026-05-29_problem/schema.sql
@@ -1,1 +1,21 @@
- 
+DROP TABLE IF EXISTS payment_attempts;
+DROP TABLE IF EXISTS order_events;
+
+CREATE TABLE payment_attempts (
+    attempt_id INT PRIMARY KEY,
+    order_id INT,
+    user_id INT,
+    gateway_id VARCHAR(10),
+    attempt_time DATETIME,
+    amount DECIMAL(10,2),
+    payment_status VARCHAR(20),
+    failure_reason VARCHAR(100)
+);
+
+CREATE TABLE order_events (
+    event_id INT PRIMARY KEY,
+    order_id INT,
+    event_time DATETIME,
+    event_type VARCHAR(30)
+);
+
