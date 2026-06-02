aebca8b8594c6265baf78f62e67865b8e9006aac
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-06-02 11:29:30 +0530

    Update schema.sql

--- DAY71_2026-05-30_problem/schema.sql
+++ DAY71_2026-05-30_problem/schema.sql
@@ -1,1 +1,13 @@
- 
+ DROP TABLE IF EXISTS inventory_events;
+
+CREATE TABLE inventory_events (
+    event_id INT PRIMARY KEY,
+    product_id VARCHAR(20),
+    warehouse_id VARCHAR(20),
+    event_time DATETIME,
+    event_type VARCHAR(30),
+    quantity INT,
+    reference_id VARCHAR(30)
+);
+
+
