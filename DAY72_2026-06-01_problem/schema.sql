878e90d8edaee1ad7ba1dd2b1654f56ae2a90ab5
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-06-04 11:43:33 +0530

    Update schema.sql

--- DAY72_2026-06-01_problem/schema.sql
+++ DAY72_2026-06-01_problem/schema.sql
@@ -1,1 +1,26 @@
- 
+ DROP TABLE IF EXISTS badge_logs;
+DROP TABLE IF EXISTS vpn_logs;
+DROP TABLE IF EXISTS meeting_room_logs;
+
+CREATE TABLE badge_logs (
+    log_id INT,
+    employee_id INT,
+    office_city VARCHAR(30),
+    event_type VARCHAR(20),
+    event_time DATETIME
+);
+
+CREATE TABLE vpn_logs (
+    log_id INT,
+    employee_id INT,
+    login_city VARCHAR(30),
+    event_time DATETIME
+);
+
+CREATE TABLE meeting_room_logs (
+    log_id INT,
+    employee_id INT,
+    room_id VARCHAR(20),
+    office_city VARCHAR(30),
+    event_time DATETIME
+);
