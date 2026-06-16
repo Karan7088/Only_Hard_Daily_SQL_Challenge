f87f088fa0502ca85f244dcbbd3d244464135405
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-06-16 14:13:33 +0530

    Update schema.sql

--- DAY73_2026-06-07_problem/schema.sql
+++ DAY73_2026-06-07_problem/schema.sql
@@ -1,1 +1,14 @@
- 
+ CREATE TABLE product_launches (
+    launch_id INT PRIMARY KEY,
+    impacted_product_id VARCHAR(10),
+    new_product_id VARCHAR(10),
+    launch_date DATE
+);
+
+CREATE TABLE daily_sales (
+    sale_id INT PRIMARY KEY,
+    product_id VARCHAR(10),
+    sale_date DATE,
+    units_sold INT,
+    revenue DECIMAL(10,2)
+);
